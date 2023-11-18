e00db32f036fae4ee7b58d96b8634d3a56c17f06
Dylan <dylansy123@gmail.com>
2023-11-17 22:41:20 -0500

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append("image", image);
+
+    try {
+      const response = await fetch("http://localhost:5000/predict", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+      setResult(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Baby Mood Detector</h1>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <button onClick={handleSubmit}>Detect Mood</button>
+      {result && (
+        <div>
+          <h2>Mood: {result.mood}</h2>
+          <h3>Reasons:</h3>
+          <ul>
+            {result.reasons.map((reason, index) => (
+              <li key={index}>{reason}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
